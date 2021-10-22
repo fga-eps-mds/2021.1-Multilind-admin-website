d8a006550777d8eeadeca46f28661e4664526714
F1reFinger <180121847@aluno.unb.br>
2021-10-22 00:36:55 -0300

    Feat:make language screen with style
    Co-authored-by: Carlos Rafael  <CarlosZoft@users.noreply.github.com>

--- src/pages/SignUpLanguage/index.js
+++ src/pages/SignUpLanguage/index.js
@@ -1,29 +1,41 @@
 import { Card } from '../../components'
+import { ContentLanguageService } from '../../services'
+import { useEffect, useState } from 'react'
 import './styles.scss'
 
 export function SignUpLanguage () {
+  const [lang, setlang] = useState([])
+
+  useEffect(() => {
+    const response = async () => {
+      const data = await ContentLanguageService.getAllLanguages()
+      setlang(data)
+    }
+    response()
+  }, [])
+  console.log(lang)
   return (
     <div className="container">
       <Card className="card">
-        <form>
+        <form className="form">
           <h2 className="Header-screen">Cadastro Língua</h2>
-          <div className="lingua-input">
-            <h3>Nome da Língua</h3>
-            <input placeholder="Guajá"></input>
+          <div className="input-language-fields">
+            <label className="label-class-language">Nome da Língua</label>
+            <input className="input-language-screen" placeholder="Guajá"></input>
           </div>
-          <div>
-            <h3>Localidade(s) da língua</h3>
-            <input placeholder="-2.43274, -46.71387"></input>
+          <div className="input-language-fields">
+            <label className="label-class-language">Localidade(s) da língua</label>
+            <input className="input-language-screen" placeholder="-2.43274, -46.71387"></input>
             <label className="obs-localidade">
               O valor deve ser informado em coordenadas e possuir fomato [latitude],
             [longintude]
             </label>
           </div>
-          <div>
-            <h3>Família Linguística</h3>
-            <input placeholder="familia"></input>
+          <div className="input-language-fields">
+            <label className="label-class-language">Família Linguística</label>
+            <input className="input-language-screen"></input>
           </div>
-          <button className="button-primary">{'Salvar'}</button>
+          <button className="button-next-page button-primary">{'Próxima'}</button>
         </form>
       </Card>
     </div>
