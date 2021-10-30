51e78c18b1958dfbad64371d0e413922b3d64b42
CarlosZoft <carlosoftwar@gmail.com>
2021-10-30 00:32:40 -0300

    feat: create service for register new language
    Co-authored-by: Helder <180121847@aluno.unb.br>

--- src/services/contentServices/contentLocationService.js
+++ src/services/contentServices/contentLocationService.js
@@ -1,8 +1,8 @@
 import { apiContent } from '../../config'
 
 export class ContentLocationService {
-  static async create (body) {
-    const response = await apiContent.post('localidade', body)
+  static async createManyLocation (body) {
+    const response = await apiContent.post('localidade/many', body)
     return response.data
   }
 }
