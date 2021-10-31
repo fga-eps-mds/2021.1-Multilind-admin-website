8fcee01db1ff1d3716c7c9b022a05d4b7dd1e33e
hericklima22 <herick.lima77@gmail.com>
2021-10-30 22:08:33 -0300

    feat: word api changed
    Co-authored-by: Helder <180121847@aluno.unb.br>
    Co-authored-by: Carlos Rafael <carlosoftwar@gmail.com>

--- src/pages/Word/ServiceWord/index.js
+++ src/pages/Word/ServiceWord/index.js
@@ -1,7 +1,13 @@
-import { ContentWordsServices } from '../../../services'
+import { ContentWordService } from '../../../services'
 
 const createWord = async (data) => {
-    const { nome, significado, lingua } = data;
+  const { palavra, lingua } = data
 
-
+  if (palavra) {
+    if (palavra.__isNew__) {
+      ContentWordService.create(lingua.id_lingua, { nome: palavra.nome, significado: palavra.significado })
+    }
+  }
 }
+
+export default createWord
--- src/services/contentServices/contentWordsService.js
+++ src/services/contentServices/contentWordsService.js
@@ -1,14 +1,13 @@
 import { apiContent } from '../../config'
 
 export class ContentWordService {
-    static async getAllWords () {
-        const response = await apiContent.get('palavra')
-        return response.data
-    }
-    
-    static async createWord (body) {
-        const response = await apiContent.post('palavra', body)
-        return response.data
-    }
-    
+  static async getAllWords () {
+    const response = await apiContent.get('palavra')
+    return response.data
+  }
+
+  static async createWord (body) {
+    const response = await apiContent.post('palavra', body)
+    return response.data
+  }
 }
