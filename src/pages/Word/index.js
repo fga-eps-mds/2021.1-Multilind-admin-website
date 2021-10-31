ae810e80d5b4ba7d49ec65b94c7ff16ac0467b4b
hericklima22 <herick.lima77@gmail.com>
2021-10-30 22:06:52 -0300

    feat: word selection changed
    Co-authored-by: Helder <180121847@aluno.unb.br>

--- src/pages/Word/index.js
+++ src/pages/Word/index.js
@@ -1,4 +1,4 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 import CreatableSelect from 'react-select/creatable'
 import { Card } from '../../components'
 // import { useHistory, useLocation } from 'react-router-dom'
@@ -9,6 +9,38 @@ import Styles from './styles/customStylesSelect'
 import './styles.scss'
 
 export function Word () {
+  const [value, setValue] = useState([])
+  const [inputValue, setInputValue] = useState([])
+
+  const createOption = (label) => ({
+    label,
+    value: label
+  })
+
+  const handleChange = (value) => {
+    setValue(value)
+  }
+
+  const handleInputChange = (inputValue) => {
+    setInputValue(inputValue)
+  }
+
+  const handleKeyDown = (event) => {
+    if (!inputValue) return
+    switch (event.key) {
+      case 'Enter':
+      case 'Tab':
+        setInputValue('')
+        if (!value.map((item) => item.label).includes(inputValue)) {
+          console.log(value.labe)
+          setValue([...value, createOption(inputValue)])
+        } else {
+          alert('Palavra já adicionada')
+        }
+        event.preventDefault()
+    }
+  }
+
   return (
     <div className='container label'>
       <Card className='card-language'>
@@ -22,10 +54,17 @@ export function Word () {
           <div className='input-language-fields div-palavra'>
             <label className='label-class-language'>{'Palavras'}</label>
             <CreatableSelect
+              inputValue={inputValue}
+              value={value}
+              components={{ DropdownIndicator: null }}
+              onInputChange={handleInputChange}
+              onChange={handleChange}
+              onKeyDown={handleKeyDown}
               isClearable
-              placeholder=''
+              placeholder='Palavra, Significado'
               className='space-between-components'
               styles={Styles}
+              menuIsOpen={false}
               isMulti
             />
           </div>
