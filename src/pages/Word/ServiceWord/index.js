541ab8dc27430a6d407e40f210fbcdd447b4302a
gabrieldvpereira <gdavi.gd@gmail.com>
2021-11-06 18:53:19 -0300

    refactor: remove logs

--- src/pages/Word/ServiceWord/index.js
+++ src/pages/Word/ServiceWord/index.js
@@ -2,7 +2,6 @@ import { ContentWordService } from '../../../services'
 
 const submitWord = async (data) => {
   const { palavra, significado, idLingua } = data
-  console.log(palavra, significado, idLingua)
   if (palavra) {
     await ContentWordService.createWord({ id_lingua: idLingua, nome: palavra, significado })
   }
