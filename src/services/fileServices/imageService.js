aa9ed6b233e0c33b5ba10da41f7354ced9519c05
CarlosZoft <carlosoftwar@gmail.com>
2021-11-06 21:56:40 -0300

    fix: security rules and code smells

--- src/services/fileServices/imageService.js
+++ src/services/fileServices/imageService.js
@@ -2,8 +2,6 @@ import { apiFiles } from '../../config/files-api'
 
 export class ImageService {
   static async create (idPalavra, formData) {
-    console.log(idPalavra, formData)
-    const response = await apiFiles.post(`/api/files/${idPalavra}`, formData)
-    return response
+    return apiFiles.post(`/api/files/${idPalavra}`, formData)
   }
 }
