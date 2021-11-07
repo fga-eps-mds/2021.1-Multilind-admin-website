af0e8e0ef41e4797abc94b692589015ca81ef15d
CarlosZoft <carlosoftwar@gmail.com>
2021-11-06 21:54:20 -0300

    fix: security rules and code smells

--- src/config/files-api.js
+++ src/config/files-api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const apiFiles = axios.create({
-  baseURL: 'http://localhost:5000/',
+  baseURL: process.env.REACT_APP_FILES_URL,
   headers: {
     'Content-Type': 'application/json'
   }
